8479c497c260ed41ad10e652b93d7280420167b7
RaihanJahat <cidcank4@gmail.com>
2024-02-05 13:04:39 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./component/Navbar/Navbar"
+
+function App() {
+  return(
+    <>
+      <Navbar />
+      <h1>Selamat Datang di Indomaret</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/component/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+function Navbar() {
+    return (
+        <>
+            <h1>RPL APP</h1>
+            <ul>
+                <li>Menu 1</li>
+                <li>Menu 2</li>
+                <li>Menu 3</li>
+            </ul>
+        </>
+    )
+}
+
+export default Navbar
